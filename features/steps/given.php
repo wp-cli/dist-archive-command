ea6146870fe882bfa8144c6b7aa3cf685b69c770
Daniel Bachhuber <daniel@handbuilt.co>
2017-05-03 07:38:48 -0700

    Update package tests and README for the move

--- features/steps/given.php
+++ features/steps/given.php
@@ -25,6 +25,13 @@ $steps->Given( '/^an? ([^\s]+) file:$/',
 	}
 );
 
+$steps->Given( '/^"([^"]+)" replaced with "([^"]+)" in the ([^\s]+) file$/', function( $world, $search, $replace, $path ) {
+	$full_path = $world->variables['RUN_DIR'] . "/$path";
+	$contents = file_get_contents( $full_path );
+	$contents = str_replace( $search, $replace, $contents );
+	file_put_contents( $full_path, $contents );
+});
+
 $steps->Given( '/^WP files$/',
 	function ( $world ) {
 		$world->download_wp();
@@ -102,7 +109,7 @@ $steps->Given( '/^download:$/',
 	}
 );
 
-$steps->Given( '/^save (STDOUT|STDERR) ([\'].+[^\'])?as \{(\w+)\}$/',
+$steps->Given( '/^save (STDOUT|STDERR) ([\'].+[^\'])?\s?as \{(\w+)\}$/',
 	function ( $world, $stream, $output_filter, $key ) {
 
 		$stream = strtolower( $stream );
@@ -120,9 +127,15 @@ $steps->Given( '/^save (STDOUT|STDERR) ([\'].+[^\'])?as \{(\w+)\}$/',
 	}
 );
 
-$steps->Given( '/^a new Phar(?: with version "([^"]+)")$/',
-	function ( $world, $version ) {
+$steps->Given( '/^a new Phar with (?:the same version|version "([^"]+)")$/',
+	function ( $world, $version = 'same' ) {
 		$world->build_phar( $version );
+	}
+);
+
+$steps->Given( '/^a downloaded Phar with (?:the same version|version "([^"]+)")$/',
+	function ( $world, $version = 'same' ) {
+		$world->download_phar( $version );
 	}
 );
 
