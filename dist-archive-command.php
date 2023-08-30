32765f65064f2f2c5bb1e794584b288d5e52a927
Pascal Birchler <pascalb@google.com>
2023-08-30 17:28:29 +0200

    Fix all autofixable CS issues

--- dist-archive-command.php
+++ dist-archive-command.php
@@ -4,7 +4,7 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
-$wpcli_dist_archive_autoloader = dirname( __FILE__ ) . '/vendor/autoload.php';
+$wpcli_dist_archive_autoloader = __DIR__ . '/vendor/autoload.php';
 if ( file_exists( $wpcli_dist_archive_autoloader ) ) {
 	require_once $wpcli_dist_archive_autoloader;
 }
